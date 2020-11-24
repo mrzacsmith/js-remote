ff96ea0830f0d91d86d760db2a807e3530c0afac
Zac Smith <zrs3141592@gmail.com>
2020-11-24 16:46:57 -0700

    basic nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
-import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  const { isAuthenticated } = useAuth0()
   return (
     <div>
       <Navbar />
